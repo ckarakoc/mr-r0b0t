7556587d8ee21444c42d6cf0cb88a2fca3ec2e9e
gotdzellal <gotdzellal@gmail.com>
2021-06-17 22:54:49 +0200

    Added dotenv.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,11 +7,10 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interaction', async interaction => {
-    if (!interaction.isCommand()) return;
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.channel.send('pong');
     }
 });
 
-client.login('token');
+client.login(process.env.BOT_TOKEN);
